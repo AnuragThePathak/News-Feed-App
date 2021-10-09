fb5f8ce749c664a7f399a2275ab7c5c2680f4642
abhishekgururani <gururani.abhishek@gmail.com>
2021-10-10 00:20:05 +0530

    feat : added Times Ago feature 	cleaned the code for added Times Ago feature

--- app/src/main/java/com/anurag/newsfeedapp/data/NewsFeedRepository.kt
+++ app/src/main/java/com/anurag/newsfeedapp/data/NewsFeedRepository.kt
@@ -25,24 +25,6 @@ class NewsFeedRepository(
                 val jsonObject = jsonArray.getJSONObject(i)
                 val publishedAt = jsonObject.getString("publishedAt")      //fetching publishedAt from the JSON file
 
-                val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")  //getting current time in specific format
-                val currentDate = sdf.format(Date())
-
-
-                val published = publishedAt.split(Regex("T|-|:|Z")) //filtering published date using regex.
-                val current = currentDate.split(Regex("/| |:")) //filtering current date using regex.
-
-
-                val daysAgo = calDays(published, current)   //calling function calDays, that returns number of days between our published and current date
-                val hoursAgo = current[3].toInt() - published[3].toInt()    //calculating hour difference
-
-                var tt : String = if(daysAgo.toInt() != 0) {
-                    "$daysAgo day ago"
-                }else {
-                    "$hoursAgo hour ago"
-                } //holds our final time difference, if it's greater than 24 than no. of daysAgo will not be 0, in that case we just need to print time diff
-
-
                 newsArray += News(
                     title = jsonObject.getString("title"),
                     url = jsonObject.getString("url"),
@@ -50,7 +32,7 @@ class NewsFeedRepository(
                     description = jsonObject.getString("description"),
                     source = jsonObject.getJSONObject("source")
                         .getString("name"),
-                        time = tt
+                        time = finalTime(publishedAt)
                 )
             }
             onSuccess(newsArray)
@@ -59,6 +41,27 @@ class NewsFeedRepository(
             onFailure(it.message)
         }
     )
+
+    private fun finalTime(publishedAt: String): String {
+
+        val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm:ss")  //getting current time in specific format
+        val currentDate = sdf.format(Date())
+
+
+        val published = publishedAt.split(Regex("T|-|:|Z")) //filtering published date using regex.
+        val current = currentDate.split(Regex("/| |:")) //filtering current date using regex.
+
+
+        val daysAgo = calDays(published, current)   //calling function calDays, that returns number of days between our published and current date
+        val hoursAgo = current[3].toInt() - published[3].toInt()    //calculating hour difference
+
+        return if(daysAgo.toInt() != 0) {
+            "$daysAgo day ago"
+        }else {
+            "$hoursAgo hour ago"
+        } //holds our final time difference, if it's greater than 24 than no. of daysAgo will not be 0, in that case we just need to print time diff
+
+    }
 
 
     //returns difference in days, between two dates, here published and current date.

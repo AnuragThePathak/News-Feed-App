32d8b200bb0b5786ed3db295449b7b213763d1d1
Rohit Jakhar <rohitjakhar940@gmail.com>
2021-10-20 17:38:25 +0530

    Add Home, Notification and Setting Fragment

---
+++ app/src/main/java/com/anurag/newsfeedapp/ui/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.anurag.newsfeedapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.anurag.newsfeedapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}

2268dfdd74081ecf6df6e16b986de9d87e2782fb
parthdhake <parthdhake@gmail.com>
2021-10-22 21:40:28 +0530

    UI of Settings Updated

--- app/src/main/java/com/anurag/newsfeedapp/ui/SettingFragment.kt
+++ app/src/main/java/com/anurag/newsfeedapp/ui/SettingFragment.kt
@@ -1,12 +1,25 @@
 package com.anurag.newsfeedapp.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.preference.PreferenceFragmentCompat
 import com.anurag.newsfeedapp.R
 
 class SettingFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey);
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        view?.setBackgroundColor((resources.getColor(R.color.dark_grey)))
+        return view
     }
 
 }

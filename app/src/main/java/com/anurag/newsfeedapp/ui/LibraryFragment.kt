8ffab05bf9acb15f3904832ca771a58074735fdc
Udit <uditverma5602@gmail.com>
2021-10-25 16:51:19 +0530

    add bottom nav bar for home, explore and library

---
+++ app/src/main/java/com/anurag/newsfeedapp/ui/LibraryFragment.kt
@@ -0,0 +1,27 @@
+package com.anurag.newsfeedapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.anurag.newsfeedapp.databinding.FragmentLibraryBinding
+
+class LibraryFragment : Fragment() {
+    private var _binding: FragmentLibraryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLibraryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
